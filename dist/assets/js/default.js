fcee6e819ae81b8ad8c90f2e31b05d3301b93e44
Roan Hahn <roan@digitalmonkey.nl>
2015-01-26 15:28:49 +0100

    Initial commit

---
+++ dist/assets/js/default.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+  $('.show-section-about').click(function()
+    {
+        $('.section-about').fadeToggle("slow");
+    });
+
+  $('.close-section-about').click(function()
+    {
+        $('.section-about').fadeToggle("slow");
+    });
+});
